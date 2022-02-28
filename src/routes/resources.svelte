a0f5cfc246aa4a3d1ab2f0542f63ac7593b4a546
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-02-28 11:36:34 -0800

    Add files via upload

--- src/routes/resources.svelte
+++ src/routes/resources.svelte
@@ -519,7 +519,7 @@
             max-width: 1980px;
             margin: 8%;
             display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+            grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
             gap: 2%;
 
             .affBox-text{
