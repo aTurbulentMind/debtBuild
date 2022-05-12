3e62c0dd0a281baa5edb81c7b3469b1ba8b7c506
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-05-12 11:34:25 -0700

    Add files via upload

--- src/routes/extra.svelte
+++ src/routes/extra.svelte
@@ -229,7 +229,7 @@ li.dropdown {
         margin:8%;
         padding: 4%;
         border-radius:4px;
-        box-shadow: 0 2px 20px 2px $shadow;
+        box-shadow: 0 2px 20px 2px $Shadows;
     }
 
     wrapper{
